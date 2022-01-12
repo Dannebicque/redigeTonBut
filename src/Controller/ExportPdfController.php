1c570b1aafc041e2465178ae1d8051ab9304ce6c
David Annebicque <david.annebicque@gmail.com>
2022-01-12 16:49:24 +0100

    feat: PdfEndroid

--- src/Controller/ExportPdfController.php
+++ src/Controller/ExportPdfController.php
@@ -9,7 +9,6 @@ use App\Repository\ApcRessourceRepository;
 use App\Repository\ApcSaeParcoursRepository;
 use App\Repository\ApcSaeRepository;
 use DateTime;
-use Endroid\Pdf\Builder\PdfBuilder;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Knp\Snappy\Pdf;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,8 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ExportPdfController extends BaseController
 {
     #[Route('/export/pdf/{parcours}', name: 'export_pdf')]
-    public function index(
-        PdfBuilder $pdfBuilder,
+    public function index(Pdf $knpSnappyPdf,
         ApcSaeRepository $apcSaeRepository,
         ApcSaeParcoursRepository $apcSaeParcoursRepository,
         ApcRessourceParcoursRepository $apcRessourceParcoursRepository,
@@ -52,51 +50,17 @@ class ExportPdfController extends BaseController
             'parcours' => $parcours,
         ]);
 
-        $pdfBuilder
-            ->setHeader([
-                'template' => 'export_pdf/_header.html.twig',
-                'cache_key' => 'header',
-                'parameters' => ['sigle' => $this->getDepartement()->getSigle(), 'parcours' => $parcours->getLibelle()]
-            ])
-            ->setFooter([
-                'template' => 'export_pdf/_footer.html.twig',
-                'cache_key' => 'footer',
-            ])
-            ->setContent([
-                'template' => 'formation/export-referentiel.html.twig',
-                'cache_key' => 'content',
-                'parameters' => [
-                    'allParcours' => $this->getDepartement()->getApcParcours(),
-                    'departement' => $this->getDepartement(),
-                    'semestres' => $semestres,
-                    'saes' => $saes,
-                    'ressources' => $ressources,
-                    'parcours' => $parcours,
-                ]
-            ])
-            ->setOptions([
+        $header = $this->renderView( 'export_pdf/_header.html.twig', ['sigle' => $this->getDepartement()->getSigle(), 'parcours' => $parcours->getLibelle()]);
+        $footer = $this->renderView( 'export_pdf/_footer.html.twig' );
 
-            ])
-        ;
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html,
 
-        $pdf = $pdfBuilder->getPdf();
-
-
-// Or output directly
-        header('Content-type: application/pdf');
-        echo $pdf->generate();
-
-//        $header = $this->renderView( 'export_pdf/_header.html.twig', ['sigle' => $this->getDepartement()->getSigle(), 'parcours' => $parcours->getLibelle()]);
-//        $footer = $this->renderView( 'export_pdf/_footer.html.twig' );
-//
-//        return new PdfResponse(
-//            $knpSnappyPdf->getOutputFromHtml($html,
-//
-//                [
-//                    'header-html' => $header,
-//                    'footer-html' => $footer,
-//            ]),
-//            $name
-//        );
+                [
+                    'header-html' => $header,
+                    'footer-html' => $footer,
+            ]),
+            $name
+        );
     }
 }
