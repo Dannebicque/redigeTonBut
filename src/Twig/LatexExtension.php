1b1f1b8a965d79cff62e3374136416fab838c12e
David Annebicque <david.annebicque@gmail.com>
2022-02-15 13:38:27 +0100

    fix: [Latex] Améliorations du rendu Latex + CLass

--- src/Twig/LatexExtension.php
+++ src/Twig/LatexExtension.php
@@ -22,7 +22,8 @@ class LatexExtension extends AbstractExtension
 
     public function markdownToLatex($text)
     {
-        $text = str_replace(['•	','<br/>','<br>'], ['* ',"\r\n\r\n","\r\n\r\n"], $text);
+        $text = nl2br($text);
+        $text = str_replace(['•	','<br/>', '<br />','<br>'], ['* ',"\r\n\r\n","\r\n\r\n","\r\n\r\n"], $text);
         $parse = new Parsedown();
         $text = $parse->text($text);
         $text = str_replace(
@@ -37,72 +38,15 @@ class LatexExtension extends AbstractExtension
 //        $text = str_replace(
 //            ['&', 'œ', '’','•','â','à','é','è','ê','ë','î','ï','ô','ù','û','ü','ç'],
 //            ['\&', '\oe{}', '\'','*','\\^a','\\`a','\\\'e','\\`e','\\^e','\\"e','\\^i','\\"i','\\^o','\\`u','\\^u','\\"u','\\c{c}'], $text);
-        $text = str_replace(
+       return str_replace(
             ['&', 'œ', '’','•','–'],
             ['\&', '\oe{}', '\'','*','--'], $text);
-
-        return $text;
-        // Latex pour le "gras"
-//        $pat1 = '/(^\\*\\*)+/m';
-//        $pat2 = '/(\\*\\*\\s)+/m';
-//        $pat3 = '/(\\*\\*$)+/m';
-//        $text = preg_replace($pat1, "\\textbf{", $text);
-//        $text = preg_replace($pat2, "} ", $text);
-//        $text = preg_replace($pat3, "} ", $text);
-
-//        $tab = explode("\n", $text);
-//        $tab2 = [];
-//        $i = 0;
-//        $start = false;
-//        foreach ($tab as $key => $value) {
-//            $ligne = ltrim($value);
-//            if ($ligne !== '') {
-//                if ($ligne[0] === '*' || $ligne[0] === '•') {
-//                    if ($start === false) {
-//                        $start = true;
-//                        $tab2[$i] = '\begin{itemize}';
-//                        $i++;
-//                    }
-//                    $tab2[$i] = '\\item ' . substr($ligne, 1);
-//                    $i++;
-//                } else {
-//                    if ($start === true) {
-//                        $start = false;
-//                        $tab2[$i] = '\end{itemize}';
-//                        $i++;
-//                        $tab2[$i] = "\n\n";
-//                        $i++;
-//                    }
-//                    $tab2[$i] = $value;
-//                    $i++;
-//                }
-//            } else {
-//                if ($start === true) {
-//                    $start = false;
-//                    $tab2[$i] = '\end{itemize}';
-//                    $i++;
-//                    $tab2[$i] = "\n\n";
-//                    $i++;
-//                } else if ($i > 1 && $tab2[$i-1] !== "\n\n") {
-//                    $tab2[$i] = "\n\n";
-//                    $i++;
-//                }
-//            }
-//        }
-//
-//        if ($start === true) {
-//            $tab2[$i] = '\end{itemize}';
-//            $i++;
-//            $tab2[$i] = "\n\n";
-//
-//        }
-
-      //  return implode("\n", $tab2);
-        // Fin latex pour le "gras"
     }
 
     public function keyWords($text)
     {
-        return str_replace([', ', ';', ',', '-','–'], [' -- ',' -- ',' -- ',' -- ',' -- '], $text);
+        $text = str_replace(['-','–'], [' -- ',' -- '], $text);
+        $text = str_replace(['&', 'œ', '’','•',', ', ';', ','], ['\&', '\oe{}', '\'','*',' -- ',' -- ',' -- '], $text);
+        return $text;
     }
 }
