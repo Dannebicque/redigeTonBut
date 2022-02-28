ac79e1ac04c14b94fc1dfaa67d46f50d043a0444
David Annebicque <david.annebicque@gmail.com>
2022-02-28 21:39:00 +0100

    feat: [Export PDF] Export Latex d'une fiche ressource ou SAE

--- src/Classes/MyPdfLatex.php
+++ src/Classes/MyPdfLatex.php
@@ -24,11 +24,15 @@ class MyPdfLatex
         $output = $this->kernel->getProjectDir() . '/public/pdf/'.$ressource->getDepartement()->getNumeroAnnexe().'/';
         $fichierLatex = $this->genereFileRessource->genereFile($ressource, $output);
 
-        sleep(3);
+        sleep(2);
         $name = 'PN-BUT-' . $ressource->getDepartement()->getSigle().'-'.$ressource->getSlugName();
 //        echo 'php '.$this->kernel->getProjectDir().'/pdf/compileLatex.php ' .$ressource->getDepartement()->getNumeroAnnexe().' '.$fichierLatex;
 //        die();
-        $text = shell_exec('php '.$this->kernel->getProjectDir().'/public/pdf/compileLatex.php ' .$ressource->getDepartement()->getNumeroAnnexe().' '.$fichierLatex.' '.$this->kernel->getProjectDir().'/public/pdf/' .$ressource->getDepartement()->getNumeroAnnexe());
+        $text = shell_exec('php '.$this->kernel->getProjectDir().'/public/pdf/compileLatex.php '.$fichierLatex.' '.$this->kernel->getProjectDir().'/public/pdf/' .$ressource->getDepartement()->getNumeroAnnexe());
+
+        sleep(3);
+//        'php /var/www/redigeTonBut/public/pdf/compileLatex.php /var/www/redigeTonBut/public/pdf/19/PN-BUT-MMI-R3-02.tex /var/www/redigeTonBut/public/pdf/19/
+
 
         $response = new Response(file_get_contents($output . $name . '.pdf'));
         $response->headers->set('Content-Type', 'application/pdf');
