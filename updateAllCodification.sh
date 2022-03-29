5a954e0660fadf26250c38741e537bf980ffc7db
David Annebicque <david.annebicque@gmail.com>
2022-03-29 04:37:39 +0200

    feat: [Codification]

---
+++ updateAllCodification.sh
@@ -0,0 +1,29 @@
+#echo "Mise à jour de la codification"
+#bin/console app:update-codification-ref
+
+echo "Mise  à jour des fichier Latex"
+bin/console app:update-codification GEA
+bin/console app:update-codification GEII
+bin/console app:update-codification GMP
+bin/console app:update-codification TC
+bin/console app:update-codification CJ
+bin/console app:update-codification CS
+bin/console app:update-codification Info-Com
+bin/console app:update-codification Chimie
+bin/console app:update-codification GB
+bin/console app:update-codification GCGP
+bin/console app:update-codification GCCD
+bin/console app:update-codification INFO
+bin/console app:update-codification MP
+bin/console app:update-codification MT2E
+bin/console app:update-codification MMI
+bin/console app:update-codification GACO
+bin/console app:update-codification PEC
+bin/console app:update-codification QLIO
+bin/console app:update-codification MLT
+bin/console app:update-codification STID
+bin/console app:update-codification GIM
+bin/console app:update-codification HSE
+bin/console app:update-codification RT
+bin/console app:update-codification SGM
+echo "Fin  de la mise  à jour des fichier Latex"
