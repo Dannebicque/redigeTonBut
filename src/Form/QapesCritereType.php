b45d951769df62e10f29be2c1af05d43af3f8520
davidannebicque <david.annebicque@gmail.com>
2023-02-12 17:38:07 +0100

    feat: Libellé pour l'affichage des critères

--- src/Form/QapesCritereType.php
+++ src/Form/QapesCritereType.php
@@ -7,6 +7,7 @@ use App\Form\Type\CollectionStimulusType;
 use phpDocumentor\Reflection\Types\Collection;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,8 +16,19 @@ class QapesCritereType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('description')
+            ->add('libelle', TextType::class, [
+                'label' => 'Libellé du critère',
+            ])
+            ->add('libelleAffichage', TextType::class, [
+                'label' => 'Libellé d\'affichage',
+                'help' => 'Libellé affiché dans les graphiques et le détail de la fiche SAE',
+                'required' => false,
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'help' => 'Description du critère affiché dans le formulaire de collecte',
+                'required' => false,
+            ])
             ->add('qapesCritereReponses', CollectionStimulusType::class, [
                 'entry_type' => QapesCritereReponseType::class,
                 'entry_options' => ['label' => false],
